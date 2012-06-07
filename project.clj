da8aa59a555618074d5fa3c5e59029b4ca59edf1
Shawn Lewis <shlewis@gmail.com>
2012-06-07 13:03:56 -0700

    Add cljs dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,15 @@
 (defproject visdb "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.1"]
+                           [jayq "0.1.0-SNAPSHOT"]
+                           [crate "0.1.0-SNAPSHOT"]
+                           [fetch "0.1.0-SNAPSHOT"]]
+            :cljsbuild {:source-path "src" 
+                        :compiler
+                          {:output-dir "resources/public/cljs/"
+                           :output-to "resources/public/cljs/core.js"
+                           :optimizations :simple
+                           :pretty-print true}} 
+            :plugins [[lein-cljsbuild "0.2.1"]]
             :main visdb.server)
-
