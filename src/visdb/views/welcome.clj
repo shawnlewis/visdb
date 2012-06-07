42de4db12765dccd0b81b7a647799cccd14f2a30
Shawn Lewis <shlewis@gmail.com>
2012-06-07 12:56:49 -0700

    Initial checkin

---
+++ src/visdb/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns visdb.views.welcome
+  (:require [visdb.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to visdb"]))
