10cf3d22c0460ac873841dd35c906f4e1050f5f0
Shawn Lewis <shlewis@gmail.com>
2012-06-09 12:02:10 -0700

    Define elements that can be dropped in the page

--- src/visdb/client/main.cljs
+++ src/visdb/client/main.cljs
@@ -3,11 +3,21 @@
     (:require [crate.core :as crate])
     )
 
+(defn log [obj]
+    (.log js/console (pr-str obj)))
+
 ;(js/alert "hey!")
 
 (def $elements ($ :#elements))
 
-(append $elements (crate/html [:p "Hello"]))
+(def textbox-element [:input {:type "text"}])
+
+(def radio-element [:input {:type "radio"}])
+
+(def elements [textbox-element radio-element])
+
+(doseq [e elements]
+    (append $elements (crate/html e)))
 
 ; Elements pane:
 ;    - text box, check box, that can be dragged to page
