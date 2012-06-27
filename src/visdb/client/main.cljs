8c78557c90136a114b83866708132658715c3cae
Shawn Lewis <shlewis@gmail.com>
2012-06-26 20:30:35 -0700

    Use a (< barebones) relational db to store state.

--- src/visdb/client/main.cljs
+++ src/visdb/client/main.cljs
@@ -1,6 +1,7 @@
 (ns visdb.client.main
     (:use [jayq.util :only [clj->js]])
-    (:require [jayq.core :as jayq]
+    (:require [visdb.client.model :as model]
+              [jayq.core :as jayq]
               [crate.core :as crate]
               [clojure.browser.repl :as repl]
               [goog.fx.DragDrop :as DragDrop]
@@ -27,44 +28,25 @@
 (def $control-templates (jayq/$ :#control-templates))
 (def $record (jayq/$ :#record))
 
-(def textbox-control
-    {:template-html [:input.draggable {:type "text"}]})
-
-(def radio-control
-    {:template-html [:input.draggable {:type "radio"}]})
-
-(def image-control
-    {:template-html [:img.draggable {:src "/img/goog-icon.png"}]})
-
 (def controls
-    (let [controls [textbox-control radio-control image-control]]
-        (for [c controls]
-            (assoc c
-                   :template-el
-                   (crate/html (:template-html c))))))
+    (let [controls
+          {:text-box {:template-html [:input.draggable {:type "text"}]}
+           :radio {:template-html [:input.draggable {:type "radio"}]}
+           :image {:template-html
+                   [:img.draggable {:src "/img/goog-icon.png"}]}}
+          add-el (fn [controls key]
+                    (assoc-in controls [key :template-el]
+                        (crate/html (get-in controls [key :template-html]))))]
 
-(doseq [c controls]
+        (reduce add-el controls (keys controls))))
+
+(doseq [c (vals controls)]
     (jayq/append $control-templates (:template-el c)))
 
 ;;; enable drag and drop
 
 
 (def records (atom []))
-(def cur-record-index (atom nil))
-(defn cur-record [] (nth @records @cur-record-index))
-(defn update-cur-record! [record]
-    (set-atom! records (assoc @records @cur-record-index record)))
-
-(defn set-atom! [atom val]
-    (swap! atom (fn [] val)))
-
-(defn add-record []
-    (let [record []]
-        (set-atom! records (conj @records record))
-        (set-atom! cur-record-index (dec (count @records)))))
-
-(defn add-control [record control coord]
-    (conj record [control coord]))
 
 (defn render []
     (let [record-list (jayq/$ "#record-list")]
@@ -75,34 +57,36 @@
                     (jayq/text (str "record" i))
                     (jayq/data "record" i))))
         (jayq/empty $record)
-        (doseq [[control coord] (cur-record)]
-            (let [new-el (crate/html (:template-html control))]
+        (doseq [{control-type :control-type position :position}
+                (get-in @model/db [:relations "field-template" :records])]
+            (let [control (controls control-type)
+                  new-el (crate/html (:template-html control))]
                 (set-css (jayq/$ new-el)
                     {"position" "absolute"
-                     "left" (str coord.x "px")
-                     "top" (str coord.y "px")})
+                     "left" (str position.x "px")
+                     "top" (str position.y "px")})
                 (jayq/append $record new-el)))))
 
-(add-record)
-(add-record)
+;(add-record)
+;(add-record)
 
-(render)
+;(render)
 
-(jayq/bind (jayq/$ "#add-record") :click
-    #(do (add-record) (render)))
-
-(jayq/on
-    (jayq/$ "#record-list-pane")
-    :click
-    "li"
-    #(do
-        (set-atom! cur-record-index
-            (-> %
-                .-target
-                jayq/$
-                (jayq/data "record")))
-        (render)))
-
+;(jayq/bind (jayq/$ "#add-record") :click
+;    #(do (add-record) (render)))
+;
+;(jayq/on
+;    (jayq/$ "#record-list-pane")
+;    :click
+;    "li"
+;    #(do
+;        (set-atom! cur-record-index
+;            (-> %
+;                .-target
+;                jayq/$
+;                (jayq/data "record")))
+;        (render)))
+;
 (defn make-drag-drop
     ([el]
       (goog.fx.DragDrop. el))
@@ -115,7 +99,10 @@
         (doseq [d drags] (.init d))
         ))
 
-(def drags (map #(make-drag-drop (:template-el %) %) controls))
+(def drags
+    (map #(make-drag-drop (get-in controls [% :template-el]) %)
+         (keys controls)))
+
 (def drop (make-drag-drop "record"))
 (drag-drop drags drop)
 
@@ -130,7 +117,7 @@
     (.css $elem k v)))
 
 (defn on-drop [event]
-    (let [control event.dragSourceItem.data
+    (let [control-type event.dragSourceItem.data
           drop-el event.dropTargetElement
           drop-client-coord (make-coord event.clientX event.clientY)
           mouse-offset (coord-difference
@@ -143,11 +130,8 @@
                         mouse-offset)
           ]
         (do
-            (update-cur-record! (add-control (cur-record) control rel-coord))
+            (model/! model/db model/insert "field-template"
+                 {:control-type control-type :position rel-coord})
             (render))))
 
 (events/listen drop "drop" on-drop)
-
-; Elements pane:
-;    - text box, check box, that can be dragged to page
-;
---
+++ src/visdb/client/model.cljs
@@ -0,0 +1,27 @@
+(ns visdb.client.model)
+
+(defn create-database [] {:id 0 :relations {}})
+
+(defn add-relation [database relname & attrs]
+    (let [schema (conj (set (map keyword attrs)) :id)]
+        (assoc-in database [:relations relname] 
+               {:schema schema 
+                :records []})))
+
+(defn insert [database relname record]
+    (let [new-id (inc (database :id))
+          with-id (assoc record :id new-id)
+          records (get-in database [:relations relname :records])
+          new-records (conj records with-id)]
+    (assoc (assoc-in database [:relations relname :records] new-records)
+           :id new-id)))
+
+(defn set-atom! [atom val]
+    (swap! atom (fn [] val)))
+
+(def db (atom (create-database)))
+
+(defn ! [db-atom fn & args]
+    (set-atom! db-atom (apply fn @db-atom args)))
+
+(! db add-relation "field-template" :control-type :position)
